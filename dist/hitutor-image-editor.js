d975cbf9f097146e90e2b06937135ec1bf0777a3
coolman200 <johnnyefa1@hitutoracdm.com>
2021-12-30 11:33:35 +0800

    update filter.js component.

--- dist/hitutor-image-editor.js
+++ dist/hitutor-image-editor.js
@@ -6534,8 +6534,6 @@ var _tuiCodeSnippet = __webpack_require__(/*! tui-code-snippet */ "tui-code-snip
 
 var _fabric = __webpack_require__(/*! fabric */ "fabric");
 
-var _fabric2 = _interopRequireDefault(_fabric);
-
 var _component = __webpack_require__(/*! @/interface/component */ "./src/js/interface/component.js");
 
 var _component2 = _interopRequireDefault(_component);
@@ -6572,7 +6570,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 */
 
 
-var filters = _fabric2.default.Image.filters;
+var filters = _fabric.fabric.Image.filters;
 
 
 filters.Mask = _mask2.default;
@@ -6764,7 +6762,7 @@ var Filter = function (_Component) {
       var filterObj = void 0;
       // capitalize first letter for matching with fabric image filter name
       var fabricType = this._getFabricFilterType(type);
-      var ImageFilter = _fabric2.default.Image.filters[fabricType];
+      var ImageFilter = _fabric.fabric.Image.filters[fabricType];
       if (ImageFilter) {
         filterObj = new ImageFilter(options);
         filterObj.options = options;
