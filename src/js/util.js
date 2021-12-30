b1a753f257521ce63d92c5aa5d95a657064ed2df
coolman200 <johnnyefa1@hitutoracdm.com>
2021-12-30 10:54:17 +0800

    update library.

--- src/js/util.js
+++ src/js/util.js
@@ -3,7 +3,6 @@
  * @fileoverview Util
  */
 import { forEach, sendHostname, extend, isString, pick, inArray } from 'tui-code-snippet';
-import Promise from 'core-js-pure/features/promise';
 import {
   commandNames,
   filterType,
@@ -18,11 +17,9 @@ const CSS_PREFIX = 'tui-image-editor-';
 const { min, max } = Math;
 let hostnameSent = false;
 
-/**
- * Export Promise Class (for simplified module path)
- * @returns {Promise} promise class
- */
-export { Promise };
+export function isFunction(value) {
+  return typeof value === 'function';
+}
 
 /**
  * Clamp value
