7046704ede2d0dff7aac54004e5e4706cf38abe2
coolman200 <johnnyefa1@hitutoracdm.com>
2021-12-30 11:39:40 +0800

    update icon component.

--- src/js/component/icon.js
+++ src/js/component/icon.js
@@ -122,7 +122,7 @@ class Icon extends Component {
       icon.set(
         snippet.extend(
           {
-            type: 'path',
+            type: 'icon',
             fill: this._oColor,
           },
           selectionStyle,
